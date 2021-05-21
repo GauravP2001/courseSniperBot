34b899f7b659eed249d408408c2782768ab7932b
Gaurav Patel <patelgaurav7480@yahoo.com>
2021-05-21 19:29:20 -0400

    Bot works with try-catch exception

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ import os
 import requests
 import asyncio
 import psycopg2
+import logging
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from discord.ext import commands
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    datefmt=f"%m/%d/%Y %H:%M:%S %Z")
+logger = logging.getLogger("Snipe Bot")
+
 
 client = commands.Bot(command_prefix=".")
 scheduler = AsyncIOScheduler()
@@ -15,50 +21,46 @@ DATABASE_URL = os.environ.get("DATABASE_URL")
 conn = psycopg2.connect(DATABASE_URL, sslmode="require")
 cur = conn.cursor()
 
-# with conn:
+with conn:
     # cur.execute("CREATE TABLE coursesToBeFound (index VARCHAR primary key);")
-    # cur.execute("INSERT INTO coursesToBeFound (index) VALUES (%s)", ("00150",))
+    cur.execute("INSERT INTO coursesToBeFound (index) VALUES (%s)", ("00150",))
     # cur.execute("DELETE FROM coursesToBeFound where index = %s", ("00150",))
     # cur.execute("SELECT * from coursesToBeFound;")
     # for row in cur:
     #     print(row[0])
 
-
-
-
-sections = ["00149", "01212", "00150"]
 sectionsFound = []
 
 @client.event
 async def on_ready():
-    print("Bot is ready")
+    logger.info("Bot is ready")
 
 @client.command()
 async def addCourse(ctx, arg):
-    print(arg)
-    sections.append(arg)
+    logger.info(arg)
     await ctx.send("Successfully Added the Course to Snipe!")
 
     with conn:
         cur.execute("INSERT INTO coursesToBeFound (index) VALUES (%s)", (arg,))
 
-
-    for course in sections:
-        print(f"Courses: {course}")
-
-# @scheduler.scheduled_job("interval", seconds=10)
 async def check_courses():
     url = "https://sis.rutgers.edu/soc/api/openSections.json?year=2021&term=9&campus=NB"
-    dataJSON = requests.get(url).json()
+
+    try:
+        dataJSON = requests.get(url).json()
+    except Exception as e:
+        logger.info(e)
+
+        return
 
     cur.execute("SELECT * from coursesToBeFound;")
 
     for row in cur:
-        print(row)
+        logger.info(row)
         for index in dataJSON:
             if row[0] == index:
                 sectionsFound.append(index)
-                print(f"Found index: {row[0]}")
+                logger.info(f"Found index: {row[0]}")
                 await client.get_channel(841918517972172804).send(f"Found Index: {index}")
 
 
@@ -66,23 +68,8 @@ async def check_courses():
         cur.execute("DELETE FROM coursesToBeFound where index = %s", (index,))
         conn.commit()
 
-    # for course in sections:
-    #     for index in dataJSON:
-    #         if index == course:
-    #             sectionsFound.append(index)
-    #             print(f"Found: {index}")
-    #             await client.get_channel(841918517972172804).send(f"Found Index: {index}")
-    #
-    #             sections.remove(index)
-
-                # with conn:
-                #     cur.execute("DELETE FROM coursesToBeFound where index = %s", (index,))
-
-# cur.close()
-# conn.close()
-
 if __name__ == "__main__":
-    print("Starting")
+    logger.info("Starting")
     scheduler.add_job(check_courses, "interval", seconds=10)
     scheduler.start()
     client.run(os.environ.get("token"))
